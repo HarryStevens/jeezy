800cdb0ecf6d5edfaee5d14a5fbf49ca70c3210d
HarryStevens <harryjosephstevens@gmail.com>
2017-11-14 20:56:18 +0530

    Remove needless comment slash

--- src/arr/transformations/removeProperty.js
+++ src/arr/transformations/removeProperty.js
@@ -2,7 +2,7 @@ import is from "../queries/isArray";
 
 // Removes a property from every object in an array of object. Returns a new array and does not change the original one.
 // The second argument can be a string or, if you want to remove multiple properties, an array of strings.
-/// Dependencies: is
+// Dependencies: is
 export default function removeProperty(arr, property){
   var d = JSON.parse(JSON.stringify(arr));
 
